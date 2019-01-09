c0ade533ca5ad662503fc98645cce297486842ed
Christoph Franke <krito@krito.de>
2019-01-09 15:00:12 +0100

    moved buildings and combined them into a single png using jump

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ const mila = require('markdown-it-link-attributes')
 const webpack = require('webpack')
 const path = require('path')
 const yargs = require('yargs')
+const jimp = require('jimp')
 
 md.use(mila, {
   attrs: {
@@ -81,7 +82,36 @@ gulp.task('serve', () => {
   })
 })
 
-gulp.task('build', gulp.parallel(['pug', 'js', 'sass', 'static']))
+gulp.task('assets', resolve => {
+  const cols = 4
+  const images = 18
+  const rows = 5
+  const base = 128
+  const pad = (n, width, z) => {
+    z = z || '0';
+    n = n + '';
+    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+  }
+  const position = (index) => ({
+    x: base * (index % cols),
+    y: base * Math.floor(index / cols)
+  })
+
+  new jimp(base*cols, base*rows, (err, result) => {  
+    Promise.all(Array.from({length: images}, (x,i) => i)
+      .map(n => `src/assets/buildings/colony_building_${pad(n+1, 4, 0)}.png`)
+      .map(filename => jimp.read(filename)))
+      .then(images => {
+        images
+          .reduce((all, image, index) => result.composite(image, position(index).x, position(index).y), result)
+          .write('dist/images/colony-screen/buildings.png')
+      })
+      .then(() => resolve())
+      .catch(console.log)
+  })
+})
+
+gulp.task('build', gulp.parallel(['pug', 'js', 'sass', 'static', 'assets']))
 
 gulp.task('watch', gulp.parallel(['serve']), () => {
 	gulp.watch('src/pages/**/*.pug', ['pug'])
@@ -89,7 +119,8 @@ gulp.task('watch', gulp.parallel(['serve']), () => {
 	gulp.watch('src/js/**/*.json', ['js'])
 	gulp.watch('src/content/**/*.md', ['pug'])
 	gulp.watch('src/sass/**/*.scss', ['sass'])
-	gulp.watch('src/static/**/*', ['static'])
+  gulp.watch('src/static/**/*', ['static'])
+	gulp.watch('src/assets/**/*', ['assets'])
 })
 
 gulp.task('default', gulp.series(['build', 'watch']))
