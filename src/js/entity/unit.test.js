336696e04e904249ddcad665328c553d0ee921f2
Christoph Franke <krito@krito.de>
2019-02-02 23:41:52 +0100

    getting closer to stability

--- src/js/entity/unit.test.js
+++ src/js/entity/unit.test.js
@@ -1,5 +1,14 @@
+import americaLargeMap from 'data/america-large'
+
 import Unit from 'entity/unit'
 import Owner from 'entity/owner'
+import MapEntity from 'entity/map'
+
+
+beforeAll(() => {
+	MapEntity.create({ data: americaLargeMap })
+})
+
 
 test('hello world', () => {
 	expect(true).toBe(true)
--- src/js/util/message.js
+++ src/js/util/message.js
@@ -4,10 +4,10 @@ const send = text => {
 
 const log = text => {
 	console.log(`Log: ${text}`)
-	const logElement = document.querySelector('#log')
-	if (logElement) {
-		logElement.innerHTML = text
-	}
+	// const logElement = document.querySelector('#log')
+	// if (logElement) {
+	// 	logElement.innerHTML = text
+	// }
 }
 
 export default { send, log }
