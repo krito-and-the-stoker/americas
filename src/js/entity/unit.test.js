e058758513e6e0331759d4b7a408351ac2981bd1
Christoph Franke <krito@krito.de>
2019-02-03 00:39:01 +0100

    added tests

--- src/js/entity/unit.test.js
+++ src/js/entity/unit.test.js
@@ -1,6 +1,7 @@
-import americaLargeMap from 'data/america-large'
+import americaSmallMap from 'data/america-small'
 
 import polyfills from 'util/polyfills'
+import Record from 'util/record'
 
 import Unit from 'entity/unit'
 import Owner from 'entity/owner'
@@ -8,16 +9,28 @@ import MapEntity from 'entity/map'
 
 
 beforeAll(() => {
-	MapEntity.create({ data: americaLargeMap })
+	MapEntity.create({ data: americaSmallMap })
 	Owner.initialize()
 })
 
 
-test('hello world', () => {
-	expect(true).toBe(true)
+test('create/disband', () => {
+	const unit = Unit.create('caravel', { x: 0, y: 0 }, Owner.player())
+	expect(unit).toBeDefined()
+	Unit.disband(unit)
+	expect(Record.getAll('unit').length).toBe(0)
 })
 
-test('create', () => {
-	Unit.create('caravel', { x: 0, y: 0 }, Owner.player())
-	expect(true).toBe(true)
+test('strength', () => {
+	const unit = Unit.create('dragoon', { x: 0, y: 0 }, Owner.player())
+	expect(Unit.strength(unit)).toBe(3)
 })
+
+// test('load/unload', () => {
+// 	const vehicle = Unit.create('caravel', { x: 0, y: 0 }, Owner.player())
+// 	const passenger = Unit.create('settler', { x: 0, y: 0 }, Owner.player())
+// 	Unit.load(vehicle, passenger)
+// 	expect(unit.passengers[0]).toBe(passenger)
+// 	Unit.unload(vehicle, passenger)
+// 	expect(unit.passengeres.length).toBe(0)
+// })
