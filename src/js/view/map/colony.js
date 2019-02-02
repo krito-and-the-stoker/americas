d3ddcb56f35fcb654556691b18ac25dbd03fc2d8
Christoph Franke <krito@krito.de>
2019-02-02 16:49:51 +0100

    show colony size and fortification on map

--- src/js/view/map/colony.js
+++ src/js/view/map/colony.js
@@ -1,80 +1,104 @@
-import ColonyView from 'view/colony'
+import ColonyFrames from 'data/colony'
+
+import Util from 'util/util'
 import Record from 'util/record'
-import Resources from 'render/resources'
-import Foreground from 'render/foreground'
-import Click from 'input/click'
+
 import Colony from 'entity/colony'
-import Text from 'render/text'
 import Tile from 'entity/tile'
 import MapEntity from 'entity/map'
 import Owner from 'entity/owner'
 
+import Click from 'input/click'
+
+import Resources from 'render/resources'
+import Foreground from 'render/foreground'
+import Text from 'render/text'
+
+import ColonyView from 'view/colony'
+
 const TILE_SIZE = 64
-const MAP_COLONY_FRAME_ID = 53 
 
+const frame = colony => ColonyFrames.colony[Object.keys(ColonyFrames.colony)
+	.filter(frame => frame !== 'default')
+	.map(frame => Number(frame))
+	.reduce((selected, frame) => colony.colonists.length >= frame ? frame : selected, 0)]
 
-const createSprite = () => Resources.sprite('map', { frame: MAP_COLONY_FRAME_ID })
+const createSprite = colony => Resources.sprite('map', { frame: frame(colony) })
 const create = colony => {
-	const  unsubscribe = Tile.listen.discovered(MapEntity.tile(colony.mapCoordinates), discovered => {
+	const view = {}
+	view.unsubscribe = Tile.listen.discovered(MapEntity.tile(colony.mapCoordinates), discovered => {
 		if (discovered) {		
-			const sprite = createSprite(colony)
-			sprite.x = TILE_SIZE * colony.mapCoordinates.x
-			sprite.y = TILE_SIZE * colony.mapCoordinates.y
-			sprite.interactive = true
+			view.sprite = createSprite(colony)
+			view.sprite.x = TILE_SIZE * colony.mapCoordinates.x
+			view.sprite.y = TILE_SIZE * colony.mapCoordinates.y
+			view.sprite.interactive = true
 
 			const unsubscribeOwner = Owner.listen.input(colony.owner, input =>
-				input ? Click.on(sprite, () => {
+				input ? Click.on(view.sprite, () => {
 					ColonyView.open(colony)
 				}) : null)
 
-			const text = Text.create(colony.name, {
+			view.text = Text.create(colony.name, {
 				fontSize: 22,
 			})
-			text.position.x = sprite.x + TILE_SIZE / 2
-			text.position.y = sprite.y + TILE_SIZE + 10
-			text.anchor.set(0.5)
+			view.text.position.x = view.sprite.x + TILE_SIZE / 2
+			view.text.position.y = view.sprite.y + TILE_SIZE + 10
+			view.text.anchor.set(0.5)
 
-			const number = Text.create(colony.colonists.length, {
+			view.number = Text.create(colony.colonists.length, {
 				fontSize: 22,
 			})
-			number.position.x = sprite.x + TILE_SIZE / 2
-			number.position.y = sprite.y + TILE_SIZE / 2
-			number.anchor.set(0.5)
+			view.number.position.x = view.sprite.x + TILE_SIZE / 2
+			view.number.position.y = view.sprite.y + TILE_SIZE / 2
+			view.number.anchor.set(0.5)
 
 			const unsubscribeSize = Colony.listen.colonists(colony, colonists => {
-				number.text = `${colonists.length}`
+				view.number.text = `${colonists.length}`
 			})
 
-			Foreground.addTerrain(sprite)
-			Foreground.addTerrain(text)
-			Foreground.addTerrain(number)
+			const unsubscribeBuildings = Colony.listen.buildings(colony, buildings => {
+				if (view.fortifications) {
+					view.fortifications.texture = Resources.texture('map', { frame: ColonyFrames.fortifications[buildings.fortifications.level] })
+				} else {
+					view.fortifications = Resources.sprite('map', { frame: ColonyFrames.fortifications[buildings.fortifications.level] })
+				}
+			})
+			view.fortifications.x = view.sprite.x
+			view.fortifications.y = view.sprite.y
+
+			Foreground.addTerrain(view.sprite)
+			Foreground.addTerrain(view.fortifications)
+			Foreground.addTerrain(view.text)
+			Foreground.addTerrain(view.number)
 
 			return () => {
-				Foreground.removeTerrain(sprite)
-				Foreground.removeTerrain(text)
-				Foreground.removeTerrain(number)
+				Foreground.removeTerrain(view.sprite)
+				Foreground.removeTerrain(view.fortifications)
+				Foreground.removeTerrain(view.text)
+				Foreground.removeTerrain(view.number)
 				unsubscribeSize()
 				unsubscribeOwner()
+				unsubscribeBuildings()
 			}
 		}	
 	})
 
-	return {
-		unsubscribe
-	}
-}
-
-const destroy = view => {
-	view.unsubscribe()
+	return view
 }
 
 const initialize = () => {
 	Record.listen('colony', colony => {
 		const view = create(colony)
 
-		return () => {
-			destroy(view)
-		}
+		const destroy = () => view.unsubscribe()
+		return Util.mergeFunctions([
+			destroy,
+			Colony.listen.colonists(colony, () => {
+				if (view.sprite) {
+					view.sprite.texture = Resources.texture('map', { frame: frame(colony) })
+				}
+			})
+		])
 	})
 }
 
