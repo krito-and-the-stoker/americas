2d269764481e6ab2927974ed7f9dfed95da1cfa1
Christoph Franke <krito@krito.de>
2019-02-06 01:43:32 +0100

    producing a vague plan

--- src/js/ai/plan.js
+++ src/js/ai/plan.js
@@ -4,40 +4,56 @@ import State from 'ai/state'
 
 const create = (state, goal) => {
 
-	const plan = {
-		goal,
-		cost: 0,
-		prev: null
-	}
 
-	// build prevs
+	// create neighbours and find leafs
+	const leafs = []
 	const search = step => {
 		step.satisfied = State.satisfies(state, step.goal)
 		if (!step.satisfied) {
 			step.prev = Object.values(Actions)
 				.filter(action => action.produces(step.goal))
 				.map(action => ({
+					next: step,
 					goal: action.needs(step.goal),
 					cost: step.cost + action.cost(state, goal),
 					name: action.name(),
 					action
 				}))
 		} else {
+			leafs.push(step)
 			step.prev = []
 		}
 
 		return step.prev
 	}
 
+
 	// build complete tree
-	let currentSteps = search(plan)
-	let i = 0
-	while(currentSteps.length > 0 && i < 10) {
+	let infinityGuard = 0
+	let currentSteps = search({
+		goal,
+		cost: 0,
+	})
+	while(currentSteps.length > 0 && infinityGuard < 10) {
 		currentSteps = currentSteps.map(step => search(step)).flat()
-		i += 1
+		infinityGuard += 1
 	}
 
-	console.log(plan)
+	if (leafs.length > 0) {
+		const start = leafs.reduce((best, step) => best.cost < step.cost ? best : step, leafs[0])
+		const plan = []
+		let step = start
+		infinityGuard = 0
+		while(step.goal !== goal && infinityGuard < 10) {
+			plan.push(step)
+			step = step.next
+			infinityGuard += 1
+		}
+
+		return plan
+	}
+
+	return null
 }
 
 export default {
