f22de2d2d7a28b3f835e5b36f1d67fa93e60b703
Christoph Franke <krito@krito.de>
2019-02-14 08:14:19 +0100

    draw order changed, farms before rivers

--- src/js/render/map.js
+++ src/js/render/map.js
@@ -31,9 +31,10 @@ class MapView{
 	assembleTile(tile) {
 		return []
 			.concat(this.renderBaseBlock(tile))
+			.concat(this.renderFarm(tile))
 			.concat(this.renderTopTiles(tile))
 			.concat(this.renderCoast(tile))
-			.concat(this.renderTerraform(tile))
+			.concat(this.renderRoads(tile))
 			.concat(this.renderBonusResources(tile))
 			.concat(this.renderUndiscovered(tile))
 	}
@@ -44,37 +45,37 @@ class MapView{
 	}
 
 	renderBaseBlock(center){
-		let indices = [];
+		let indices = []
 
 		if(!center || !center.discovered())
-			return indices;
+			return indices
 
-		let left = center.left();
-		let right = center.right();
-		let up = center.up();
-		let down = center.down();
+		let left = center.left()
+		let right = center.right()
+		let up = center.up()
+		let down = center.down()
 
 		if(up && right && down && left){		
-			let leftUp = left.up();
-			let leftDown = left.down();
-			let rightUp = right.up();
-			let rightDown = right.down();
+			let leftUp = left.up()
+			let leftDown = left.down()
+			let rightUp = right.up()
+			let rightDown = right.down()
 			if(leftUp && leftDown && rightUp && rightDown){
-				indices.push(this.getTileIndex(rightDown, -1, -1, center));
-				indices.push(this.getTileIndex(down, 0, -1, center));
-				indices.push(this.getTileIndex(leftDown, 1, -1, center));
+				indices.push(this.getTileIndex(rightDown, -1, -1, center))
+				indices.push(this.getTileIndex(down, 0, -1, center))
+				indices.push(this.getTileIndex(leftDown, 1, -1, center))
 
-				indices.push(this.getTileIndex(right, -1, 0, center));
-				indices.push(this.getTileIndex(center, 0, 0, center));
-				indices.push(this.getTileIndex(left, 1, 0, center));
+				indices.push(this.getTileIndex(right, -1, 0, center))
+				indices.push(this.getTileIndex(center, 0, 0, center))
+				indices.push(this.getTileIndex(left, 1, 0, center))
 
-				indices.push(this.getTileIndex(rightUp, -1, 1, center));
-				indices.push(this.getTileIndex(up, 0, 1, center));
-				indices.push(this.getTileIndex(leftUp, 1, 1, center));
+				indices.push(this.getTileIndex(rightUp, -1, 1, center))
+				indices.push(this.getTileIndex(up, 0, 1, center))
+				indices.push(this.getTileIndex(leftUp, 1, 1, center))
 			}
 		}
 
-		return indices;
+		return indices
 	}
 
 
@@ -83,24 +84,24 @@ class MapView{
 		if(center.terrain.domain === 'land'){
 			//either from land
 			if(other.terrain.domain === 'land')
-				return other.terrain.centerTile;
+				return other.terrain.centerTile
 			//or from coast
 			if(other.terrain.domain === 'sea' && other.coastTerrain)
-				return other.coastTerrain.centerTile;
+				return other.coastTerrain.centerTile
 			//or from self
-			return center.terrain.centerTile;
+			return center.terrain.centerTile
 		}
 
 		// coast wants land from everybody
 		if(center.terrain.domain === 'sea' && center.coastTerrain){
 			//take land from land tile
 			if(other.terrain.domain === 'land')
-				return other.terrain.centerTile;
+				return other.terrain.centerTile
 			//or from coast terrain
 			if(other.coastTerrain)
-				return other.coastTerrain.centerTile;
+				return other.coastTerrain.centerTile
 			//or from self
-			return center.terrain.centerTile;
+			return center.terrain.centerTile
 		}
 
 		// sea always wants sea
@@ -108,41 +109,41 @@ class MapView{
 			//take sea tile
 			if(other.terrain.domain === 'sea'){
 				//either coastal
-				if(other.isCoastalSea || other.coastTerrain)
-					return Terrain['coastal sea'].centerTile;
+				if(other.isCoastalSea || other.coastTerrain)
+					return Terrain['coastal sea'].centerTile
 				//or deep sea
-				return other.terrain.centerTile;
+				return other.terrain.centerTile
 			}
 			//from land tiles take coastal sea
-			return Terrain['coastal sea'].centerTile;			
+			return Terrain['coastal sea'].centerTile			
 		}
 	}
 
 	getTileIndex(other, x, y, center){
-		return this.decideLandSeaTile(center, other) + this.spriteSheetWidth*y + x;
+		return this.decideLandSeaTile(center, other) + this.spriteSheetWidth*y + x
 	}
 
 	renderUndiscovered(center){
-		let undiscovered = [];
+		let undiscovered = []
 
 		if(!center || !center.discovered())
-			return undiscovered;
+			return undiscovered
 
-		let left = center.left();
-		let right = center.right();
-		let up = center.up();
-		let down = center.down();
+		let left = center.left()
+		let right = center.right()
+		let up = center.up()
+		let down = center.down()
 
 
 		if(up && right && down && left){		
-			let name = this.neighborToName(!up.discovered(), !right.discovered(), !down.discovered(), !left.discovered());
+			let name = this.neighborToName(!up.discovered(), !right.discovered(), !down.discovered(), !left.discovered())
 			if(name !== null)
-				undiscovered.push(Terrain.undiscovered[name]);
+				undiscovered.push(Terrain.undiscovered[name])
 
-			let leftUp = left.up();
-			let leftDown = left.down();
-			let rightUp = right.up();
-			let rightDown = right.down();
+			let leftUp = left.up()
+			let leftDown = left.down()
+			let rightUp = right.up()
+			let rightDown = right.down()
 
 			if(leftUp && leftDown && rightUp && rightDown){
 				let cornerNames = this.getCornerNames(
@@ -154,38 +155,38 @@ class MapView{
 					!leftDown.discovered(),
 					!left.discovered(),
 					!leftUp.discovered()
-				);
+				)
 				for(let name of cornerNames){
-					// console.error(tile, name);
-					undiscovered.push(Terrain.undiscovered[name]);
+					// console.error(tile, name)
+					undiscovered.push(Terrain.undiscovered[name])
 				}
 							
 			}
 
 		}
 
-		return undiscovered;
+		return undiscovered
 	}
 
 	renderCoast(tile){
-		let indices = [];		
-		indices = [...indices, ...this.renderCoastLine(tile)];
-		indices = [...indices, ...this.renderCoastCorners(tile)];
-		indices = [...indices, ...this.renderCoastalSea(tile)];
+		let indices = []		
+		indices = [...indices, ...this.renderCoastLine(tile)]
+		indices = [...indices, ...this.renderCoastCorners(tile)]
+		indices = [...indices, ...this.renderCoastalSea(tile)]
 
-		return indices;
+		return indices
 	}
 
 	renderCoastalSea(center){
-		let indices = [];
+		let indices = []
 
 		if(!center)
-			return indices;
+			return indices
 
-		let left = center.left();
-		let right = center.right();
-		let up = center.up();
-		let down = center.down();
+		let left = center.left()
+		let right = center.right()
+		let up = center.up()
+		let down = center.down()
 
 		if(
 			center &&
@@ -197,36 +198,36 @@ class MapView{
 			center.terrain.domain === 'sea' &&
 			center.coastTerrain
 		){
-			let leftUp = left.up();
-			let leftDown = left.down();
-			let rightUp = right.up();
-			let rightDown = right.down();
+			let leftUp = left.up()
+			let leftDown = left.down()
+			let rightUp = right.up()
+			let rightDown = right.down()
 
 			if(leftUp && rightUp && leftDown && rightDown){
 				if(leftUp.terrain.domain === 'sea' && left.terrain.domain === 'sea' && up.terrain.domain === 'sea')
-					indices.push(Terrain['coastal sea'].northWestCorner);
+					indices.push(Terrain['coastal sea'].northWestCorner)
 				if(leftDown.terrain.domain === 'sea' && left.terrain.domain === 'sea' && down.terrain.domain === 'sea')
-					indices.push(Terrain['coastal sea'].southWestCorner);
+					indices.push(Terrain['coastal sea'].southWestCorner)
 				if(rightUp.terrain.domain === 'sea' && right.terrain.domain === 'sea' && up.terrain.domain === 'sea')
-					indices.push(Terrain['coastal sea'].northEastCorner);
+					indices.push(Terrain['coastal sea'].northEastCorner)
 				if(rightDown.terrain.domain === 'sea' && right.terrain.domain === 'sea' && down.terrain.domain === 'sea')
-					indices.push(Terrain['coastal sea'].southEastCorner);
+					indices.push(Terrain['coastal sea'].southEastCorner)
 			}
 		}
 
-		return indices;
+		return indices
 	}
 
 	renderCoastLine(center){
-		let coastTiles = [];
+		let coastTiles = []
 
 		if(!center)
-			return coastTiles;
+			return coastTiles
 
-		let left = center.left();
-		let right = center.right();
-		let up = center.up();
-		let down = center.down();
+		let left = center.left()
+		let right = center.right()
+		let up = center.up()
+		let down = center.down()
 
 		if(
 			center.discovered() &&
@@ -241,33 +242,33 @@ class MapView{
 				right.terrain.domain === 'land',
 				down.terrain.domain === 'land',
 				left.terrain.domain === 'land'
-			);
+			)
 
 			if(name)
-				coastTiles.push(Terrain.coast[name]);
+				coastTiles.push(Terrain.coast[name])
 		}
 
-		return coastTiles;
+		return coastTiles
 	}
 
 	renderCoastCorners(center){
-		let corners = [];
+		let corners = []
 
 		if(center){
 			//no corners on land tiles
 			if(center.terrain.domain === 'land' || !center.discovered())
-				return corners;
+				return corners
 
-			let left = center.left();
-			let right = center.right();
-			let up = center.up();
-			let down = center.down();
+			let left = center.left()
+			let right = center.right()
+			let up = center.up()
+			let down = center.down()
 			if(left && right && up && down){
 
-				let leftUp = left.up();
-				let leftDown = left.down();
-				let rightUp = right.up();
-				let rightDown = right.down();
+				let leftUp = left.up()
+				let leftDown = left.down()
+				let rightUp = right.up()
+				let rightDown = right.down()
 
 				if(leftUp && rightUp && leftDown && rightDown){
 					let cornerNames = this.getCornerNames(
@@ -279,34 +280,19 @@ class MapView{
 						leftDown.terrain.domain === 'land',
 						left.terrain.domain === 'land',
 						leftUp.terrain.domain === 'land'
-					);
+					)
 					for(let name of cornerNames){
-						corners.push(Terrain.coast[name]);
+						corners.push(Terrain.coast[name])
 					}
 				}
 			}
 		}
 
-		return corners;
+		return corners
 	}
 
-	renderTerraform(tile) {
-		const up = Tile.up(tile)
-		const right = Tile.right(tile)
-		const down = Tile.down(tile)
-		const left = Tile.left(tile)
-
+	renderRoads(tile) {
 		const indices = []
-		if (tile.plowed) {
-			const plowedBase = Terrain.plowed.singleTile
-			const mod = this.getForestTileModifier(
-				up.plowed,
-				right.plowed,
-				down.plowed,
-				left.plowed
-			)
-			indices.push(plowedBase + mod);
-		}
 		if (tile.road) {
 			const row = 16
 			const col = 1
@@ -349,213 +335,234 @@ class MapView{
 		return indices
 	}
 
+	renderFarm(tile) {
+		const up = Tile.up(tile)
+		const right = Tile.right(tile)
+		const down = Tile.down(tile)
+		const left = Tile.left(tile)
+
+		const indices = []
+		if (tile.plowed) {
+			const plowedBase = Terrain.plowed.singleTile
+			const mod = this.getForestTileModifier(
+				up.plowed,
+				right.plowed,
+				down.plowed,
+				left.plowed
+			)
+			indices.push(plowedBase + mod)
+		}
+
+		return indices
+	}
+
 	getCornerNames(up, rightUp, right, rightDown, down, leftDown, left, leftUp){
-		let cornerNames = [];
+		let cornerNames = []
 
 		if(leftUp && !up && !left){
-			cornerNames.push('southEastCorner');
+			cornerNames.push('southEastCorner')
 		}
 		if(leftDown && !left && !down){
-			cornerNames.push('northEastCorner');
+			cornerNames.push('northEastCorner')
 		}
 		if(rightUp && !right && !up){
-			cornerNames.push('southWestCorner');
+			cornerNames.push('southWestCorner')
 		}
 		if(rightDown && !right && !down){
-			cornerNames.push('northWestCorner');
+			cornerNames.push('northWestCorner')
 		}
 
-		return cornerNames;
+		return cornerNames
 	}
 
 	neighborToName(top, right, down, left){
 		if(top && !down && !left && !right){
-			return 'south';
+			return 'south'
 		}
 		if(!top && down && !left && !right){
-			return 'north';
+			return 'north'
 		}
 		if(!top && !down && left && !right){
-			return 'east';
+			return 'east'
 		}
 		if(!top && !down && !left && right){
-			return 'west';
+			return 'west'
 		}
 
 		if(top && !down && left && !right){
-			return 'southEast';
+			return 'southEast'
 		}
 		if(top && !down && !left && right){
-			return 'southWest';
+			return 'southWest'
 		}
 		if(!top && down && left && !right){
-			return 'northEast';
+			return 'northEast'
 		}
 		if(!top && down && !left && right){
-			return 'northWest';
+			return 'northWest'
 		}
 
 		if(!top && down && left && right){
-			return 'southBay';
+			return 'southBay'
 		}
 		if(top && !down && left && right){
-			return 'northBay';
+			return 'northBay'
 		}
 		if(top && down && !left && right){
-			return 'eastBay';
+			return 'eastBay'
 		}
 		if(top && down && left && !right){
-			return 'westBay';
+			return 'westBay'
 		}
 
 		if(top && down && left && right){
-			return 'lake';
+			return 'lake'
 		}
 		if(top && down && !left && !right){
-			return 'eastWestPassage';
+			return 'eastWestPassage'
 		}
 		if(!top && !down && left && right){
-			return 'northSouthPassage';
+			return 'northSouthPassage'
 		}
 
-		return null;
+		return null
 	}
 
 	getForestTileModifier(up, right, down, left){
-		let y = this.spriteSheetWidth;
+		let y = this.spriteSheetWidth
 
 		if(!up && !right && !down && !left)
-			return 0;
+			return 0
 
 		if(!up && right && !down && !left)
-			return 1;
+			return 1
 		if(!up && right && !down && left)
-			return 2;
+			return 2
 		if(!up && !right && !down && left)
-			return 3;
+			return 3
 
 		if(!up && !right && down && !left)
-			return 1*y;
+			return 1*y
 		if(up && !right && down && !left)
-			return 2*y;
+			return 2*y
 		if(up && !right && !down && !left)
-			return 3*y;
+			return 3*y
 
 		if(!up && right && down && !left)
-			return 1*y + 1;
+			return 1*y + 1
 		if(!up && right && down && left)
-			return 1*y + 2;
+			return 1*y + 2
 		if(!up && !right && down && left)
-			return 1*y + 3;
+			return 1*y + 3
 
 		if(up && right && down && !left)
-			return 2*y + 1;
+			return 2*y + 1
 		if(up && right && down && left)
-			return 2*y + 2;
+			return 2*y + 2
 		if(up && !right && down && left)
-			return 2*y + 3;
+			return 2*y + 3
 
 		if(up && right && !down && !left)
-			return 3*y + 1;
+			return 3*y + 1
 		if(up && right && !down && left)
-			return 3*y + 2;
+			return 3*y + 2
 		if(up && !right && !down && left)
-			return 3*y + 3;
+			return 3*y + 3
 
 	}
 
 	getBonusResourceName(tile){
 		if(tile.mountains)
-			return 'silver';
+			return 'silver'
 		if(tile.hills)
-			return 'ore';
+			return 'ore'
 
 		if(tile.name === 'plains' && !tile.forest)
-			return 'wheat';
+			return 'wheat'
 		if(tile.name === 'grassland' && !tile.forest)
-			return 'tobacco';
+			return 'tobacco'
 		if(tile.name === 'prairie' && !tile.forest)
-			return 'cotton';
+			return 'cotton'
 		if(tile.name === 'savannah' && !tile.forest)
-			return 'sugar';
+			return 'sugar'
 
 		if(tile.name === 'tundra' && tile.forest)
-			return 'game';
+			return 'game'
 		if(tile.name === 'prairie' && tile.forest)
-			return 'game';
+			return 'game'
 		if(tile.name === 'plains' && tile.forest)
-			return 'fur';
+			return 'fur'
 
 		if(tile.name  === 'grassland' && tile.forest)
-			return 'wood';
+			return 'wood'
 		if(tile.name  === 'savannah' && tile.forest)
-			return 'wood';
+			return 'wood'
 
 		if(tile.name === 'desert')
-			return 'oasis';
+			return 'oasis'
 
 		if(tile.name === 'ocean')
-			return 'fish';
+			return 'fish'
 		if(tile.name === 'coastal sea')
-			return 'fish';
+			return 'fish'
 
-		return 'minerals';
+		return 'minerals'
 	}
 
 	renderBonusResources(center){
-		let bonus = [];
+		let bonus = []
 		if(center && center.discovered() && center.bonus){
-			let resourceName = this.getBonusResourceName(center);
-			bonus.push(Terrain.bonusResource[resourceName]);
+			let resourceName = this.getBonusResourceName(center)
+			bonus.push(Terrain.bonusResource[resourceName])
 		}
 
 		if (center && center.discovered() && center.rumors){
 			bonus.push(Terrain.rumors.id)
 		}
 
-		return bonus;
+		return bonus
 	}
 
 	renderTopTiles(center){
-		let topTiles = [];
+		let topTiles = []
 		if(center && center.discovered()){
-			let left = center.left();
-			let right = center.right();
-			let up = center.up();
-			let down = center.down();
+			let left = center.left()
+			let right = center.right()
+			let up = center.up()
+			let down = center.down()
 
 			if(left && right && up && down){
 				if(center.hills){
 					const peakBase = center.hillVariation ? Terrain.hillsPeak.singleTileVariation : Terrain.hillsPeak.singleTile
-					// const baseBase = center.hillVariation ? Terrain.hillsBase.singleTileVariation : Terrain.hillsBase.singleTile;
+					// const baseBase = center.hillVariation ? Terrain.hillsBase.singleTileVariation : Terrain.hillsBase.singleTile
 					const mod = this.getForestTileModifier(
 						up.hills,
 						right.hills,
 						down.hills,
 						left.hills
-					);
-					// topTiles.push(-(baseBase + mod));
-					topTiles.push(peakBase + mod);
+					)
+					// topTiles.push(-(baseBase + mod))
+					topTiles.push(peakBase + mod)
 				}
 				if(center.mountains){
 					const peakBase = center.mountainVariation ? Terrain.mountainsPeak.singleTileVariation : Terrain.mountainsPeak.singleTile
-					// const baseBase = center.mountainVariation ? Terrain.mountainsBase.singleTileVariation : Terrain.mountainsBase.singleTile;
+					// const baseBase = center.mountainVariation ? Terrain.mountainsBase.singleTileVariation : Terrain.mountainsBase.singleTile
 					const mod = this.getForestTileModifier(
 						up.mountains,
 						right.mountains,
 						down.mountains,
 						left.mountains
-					);
-					// topTiles.push(-(baseBase + mod));
-					topTiles.push(peakBase + mod);
+					)
+					// topTiles.push(-(baseBase + mod))
+					topTiles.push(peakBase + mod)
 				}
 				if(center.riverSmall){
-					let mod = this.getForestTileModifier(up.riverSmall, right.riverSmall, down.riverSmall, left.riverSmall);
-					topTiles.push(Terrain['smallRiver'].singleTile + mod);
+					let mod = this.getForestTileModifier(up.riverSmall, right.riverSmall, down.riverSmall, left.riverSmall)
+					topTiles.push(Terrain['smallRiver'].singleTile + mod)
 				}
 				if(center.riverLarge){
-					let mod = this.getForestTileModifier(up.riverLarge, right.riverLarge, down.riverLarge, left.riverLarge);
-					topTiles.push(Terrain['largeRiver'].singleTile + mod);
+					let mod = this.getForestTileModifier(up.riverLarge, right.riverLarge, down.riverLarge, left.riverLarge)
+					topTiles.push(Terrain['largeRiver'].singleTile + mod)
 				}
 				if(center.forest && RealMapView.isForestVisible()){
 					let mod = this.getForestTileModifier(
@@ -563,17 +570,17 @@ class MapView{
 						right.forest && !(center.treeVariation && right.treeVariation),
 						down.forest && !(center.treeVariation && down.treeVariation),
 						left.forest && !(center.treeVariation && left.treeVariation)
-					);
+					)
 					const baseForestTile = [Terrain.forest.singleTile, Terrain.forest.singleTileVariation, Terrain.forest.singleTileVariation2][center.treeVariation]
-					topTiles.push(baseForestTile + mod);
+					topTiles.push(baseForestTile + mod)
 				}
 			}
 
 		}
 
-		return topTiles;
+		return topTiles
 	}
 
 }
 
-export default MapView;
+export default MapView
