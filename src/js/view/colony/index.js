9fe824ee7e8ab7ee46ff0325aadd6f0900cea57d
Christoph Franke <krito@krito.de>
2020-03-18 10:00:59 +0100

    change colony backgrount color again

--- src/js/view/colony/index.js
+++ src/js/view/colony/index.js
@@ -30,7 +30,7 @@ const create = colony => {
 	const container = new PIXI.Container()
 	// const colonyWoodBackground = new PIXI.extras.TilingSprite(Resources.texture('colonyWoodBackground'), RenderView.getDimensions().x, RenderView.getDimensions().y)
 	const colonyWoodBackground = Resources.sprite('white')
-	colonyWoodBackground.tint = 0x403506
+	colonyWoodBackground.tint = 0x382f1f
 	colonyWoodBackground.alpha = 0.7
 	window.background = colonyWoodBackground
 	
