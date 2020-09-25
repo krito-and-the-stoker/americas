25fe0e19f27d8ca866d3b9ee7850090d3ea46c65
Christoph Franke <krito@krito.de>
2020-09-25 12:20:28 +0200

    small fix

--- src/js/view/colony/storage.js
+++ src/js/view/colony/storage.js
@@ -37,14 +37,15 @@ const create = (colony, originalDimensions) => {
 			colony
 		}
 
-		const unsubscribe = args.amount > 0 ? [
-			Drag.makeDraggable(sprite, args, 'Load onto transport vehicle or equip colonist'),
+		const unsubscribe = [
+			args.amount > 0 &&
+				Drag.makeDraggable(sprite, args, 'Load onto transport vehicle or equip colonist'),
 			Click.on(sprite, () => {
 				const options = [Trade.NOTHING, Trade.IMPORT, Trade.EXPORT, Trade.BALANCE]
 				colony.trade[good] = options[(colony.trade[good] + 1) % options.length]
 				Trade.update(colony.trade)
 			}, `Set trade options for ${good}`)
-		] : null
+		]
 
 		return {
 			update,
