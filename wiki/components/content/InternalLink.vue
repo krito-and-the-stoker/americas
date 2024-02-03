4613379b9d1bcd02b02b4fe6e0467dbf2605e8d1
Christoph Franke <hallo@christophfranke.info>
2024-02-03 09:29:37 +0100

    enable internal links

---
+++ wiki/components/content/InternalLink.vue
@@ -0,0 +1,32 @@
+<script setup>
+const props = defineProps({
+	title: {
+		type: String,
+		required: true,
+	},
+});
+
+const { navigation } = useContent();
+const findPath = (node, title) => {
+	if (node.title === title) {
+		return node._path;
+	}
+
+	if (node.children) {
+		for (const child of node.children) {
+			const path = findPath(child, title);
+			if (path) {
+				return path;
+			}
+		}
+	}
+};
+
+const path = computed(() => {
+	return findPath({ children: navigation.value }, props.title);
+});
+</script>
+
+<template>
+	<nuxt-link :to="path">{{ props.title }}</nuxt-link>
+</template>
