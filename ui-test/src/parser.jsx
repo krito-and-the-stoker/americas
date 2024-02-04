e2def16ee0dd96bb549fa6646be206ee1cb21233
Christoph Franke <hallo@christophfranke.info>
2024-02-04 11:50:37 +0100

    built a somewhat working ast

--- ui-test/src/parser.jsx
+++ ui-test/src/parser.jsx
@@ -9,40 +9,247 @@ const hello =`
 
 [button action:increment] Increment []
 
-[repeat list] {.}, []
+[repeat list] {_}, []
 
 `
 
+// tries to match all matchers in their specified order
+// if one matchers is not matched it fails
+function matchAll (matchers, input) {
+	let rest = input
+	let children = []
+	for (const fn of matchers) {
+		const match = fn(rest)
+		if (!match) {
+			return null
+		}
+
+		children.push({
+			name: match.name,
+			children: match.children,
+			value: match.value
+		})
+
+		rest = match.rest
+	}
+
+	return {
+		children,
+		rest
+	}
+}
+
+// tries to match one of the matchers
+// if no matchers matches, it fails
+function matchOne(matchers, input) {
+	let match = null
+
+	// try to match any of the matchers
+	for (const fn of matchers) {		
+		match = fn(input)
+		if (match) {
+			return match
+		}
+	}
+
+	// could not match
+	return null
+}
+
+// tries to consume without matching any
+// if no character can be consumed, it fails
+function matchNone(matchers, input) {
+	let position = 0
+	while (position < input.length) {	
+		const rest = input.substring(position)
+		// if we matched something, we need to stop here
+		if (matchers.find(fn => fn(rest))) {
+			break;
+		}
+
+		// consume 1 char
+		position += 1;
+	}
+
+	// we matched something immediately
+	if (!position) {
+		return null
+	}
+
+	// report consumption
+	return {
+		value: input.substring(0, position),
+		rest: input.substring(position)
+	}
+}
+
+const tokens = {
+	template: input => {
+		let match = {
+			name: 'template',
+			value: null,
+			children: [],
+			rest: input
+		}
+
+		while (match.rest) {
+			const result = matchOne([
+				// match.function,
+				tokens.boldTag,
+				// match.italic,
+				// match.line,
+				tokens.interpolation,
+				tokens.text
+			], match.rest)
+			// console.log('template', match.rest, { result })
+
+			if (result) {
+				match.children.push({
+					name: result.name,
+					children: result.children,
+					value: result.value
+				})
+				match.rest = result.rest
+			} else {
+				// could not match template
+				return null
+			}
+		}
+
+		return match
+	},
+
+	boldToken: input => {
+		if (input.startsWith('**')) {
+			return {
+				name: 'boldToken',
+				rest: input.substring(2)
+			}
+		}
+	},
+
+	boldTag: input => {
+		const match = matchAll([
+			tokens.boldToken,
+			tokens.text,
+			tokens.boldToken,
+		], input)
+
+		if (match) {
+			return {
+				name: 'boldTag',
+				children: [match.children[1]],
+				rest: match.rest
+			}
+		}
+	},
+
+	text: input => {
+		const match = matchNone([
+			tokens.boldToken,
+			tokens.interpolationStart
+		], input)
+		// console.log('text:', input, { match })
+
+		if (match) {
+			return {
+				name: 'text',
+				value: match.value,
+				rest: match.rest
+			}
+		}
+	},
+
+	interpolationStart: input => {
+		if (input.startsWith('{')) {
+			return {
+				name: 'interpolationStart',
+				rest: input.substring(1)
+			}
+		}
+	},
+
+	interpolationEnd: input => {
+		if (input.startsWith('}')) {
+			return {
+				name: 'interpolationStart',
+				rest: input.substring(1)
+			}
+		}
+	},
+
+	variable: input => {
+		const re = /^\s*(\b[a-zA-Z_][0-9a-zA-Z_]*\b)\s*/
+		const result = input.match(re)
+		console.log('variable', input, result)
+		if (result) {
+			const all = result[0]
+			const variable = result[1]
+			return {
+				name: 'variable',
+				value: variable,
+				rest: input.substring(all.length)
+			}
+		}
+	},
+
+	interpolation: input => {
+		const match = matchAll([
+			tokens.interpolationStart,
+			tokens.variable,
+			tokens.interpolationEnd,
+		], input)
+
+		if (match) {
+			return {
+				name: 'interpolation',
+				value: match.children[1].value,
+				rest: match.rest
+			}
+		}
+	}
+}
+
+// const tokens = [{
+// 	match: '**',
+// 	name: 'bold'
+// }, {
+// 	match: '*',
+// 	name: 'italic'
+// }, {
+// 	match: /\[.+?\]/,
+// 	name: 'openFunction'
+// }, {
+// 	match: '[]',
+// 	name: 'closeFunction'
+// }, {
+// 	match: '\n',
+// 	name: 'newline'
+// }, {
+// 	match: '\t',
+// 	name: 'indentation'
+// }, {
+// 	match: '  ',
+// 	name: 'indentation'
+// }, {
+// 	match: /\{.+?\}/,
+// 	name: 'interpolation'
+// }]
 
 const tags = {
 	bold: {
 		begin: 'bold',
 		end: 'bold',
-		tokens: [{
-			match: '**',
-			name: 'bold'
-		}],
 		replace: subtree => context => <b>{subtree(context)}</b>
 	},
 	italic: {
 		begin: 'italic',
 		end:'italic',
-		tokens: [{
-			match: '*',
-			name: 'italic'
-		}],
 		replace: subtree => context => <i>{subtree(context)}</i>
 	},
 	function: {
 		begin: 'openFunction',
 		end: 'closeFunction',
-		tokens: [{
-			match: /\[.+?\]/,
-			name: 'openFunction'
-		}, {
-			match: '[]',
-			name: 'closeFunction'
-		}],
 		replace: (subtree, params) => {
 			const fn = params[0]
 			const paramObject = {
@@ -67,33 +274,17 @@ const tags = {
 	},
 	newline: {
 		is: 'newline',
-		tokens: [{
-			match: '\n',
-			name: 'newline'
-		}],
 		replace: () => () => <br />
 	},
 	indentation: {
 		is: 'indentation',
-		tokens: [{
-			match: '\t',
-			name: 'indentation'
-		}, {
-			match: '  ',
-			name: 'indentation'
-		}]
 	},
 	interpolate: {
 		is: 'interpolation',
-		tokens: [{
-			match: /\{.+?\}/,
-			name: 'interpolation'
-		}],
 		replace: (subtree, params) => context => context[params[0].trim()]
 	}
 }
 
-const tokens = Object.values(tags).flatMap(tag => tag.tokens)
 
 const matchStart = (str, expr) => {
 	if (typeof expr === 'string') {
@@ -188,7 +379,7 @@ const defaultContext = {
 }
 
 export default () => {
-	const helloTokens = tokanize(hello)
+	const helloTokens = tokens.template(hello)
 	console.log(helloTokens)
 	// const render = parse(hello)
 	// return context => console.log('render') || render({
