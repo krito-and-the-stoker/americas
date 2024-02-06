0a689bdff2c27524bb66bbce1acfa5269d61b8d8
Christoph Franke <hallo@christophfranke.info>
2024-02-06 19:13:35 +0100

    added store to show reactivity of templates

--- game/src/ui/Main.jsx
+++ game/src/ui/Main.jsx
@@ -1,4 +1,5 @@
 import { createSignal } from 'solid-js'
+import { createStore } from 'solid-js/store'
 
 import Dialog from './Dialog'
 
@@ -8,17 +9,19 @@ function registerDialogFunctions (setName, setContext) {
   window.openDialog = (name, context = {}) => {
     setContext(context)
     setName(name)
+
+    return setContext
   }
   window.closeDialog = () => setName('')
 }
 
 function Main() {
   const [name, setName] = createSignal('')
-  const [context, setContext] = createSignal({})
+  const [context, setContext] = createStore({})
   registerDialogFunctions(setName, setContext)
 
   return <>
-    <Dialog name={name()} context={context()} />
+    <Dialog name={name()} context={context} />
   </>
 }
 
