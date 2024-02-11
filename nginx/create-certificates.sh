17ea22a1357dfbe97ce47933e1cdf7965989514e
Christoph Franke <hallo@christophfranke.info>
2024-02-11 09:58:49 +0100

    updates for certificate

--- nginx/create-certificates.sh
+++ nginx/create-certificates.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
 # Configuration variables
-DOMAIN="articles.krito.de"
+DOMAIN="play-americas.com"
+SUBDOMAINS="www.play-americas.com wiki.play-americas.com dashboard.play-americas.com"
 EMAIL="public@krito.de"
-WEBROOT_PATH="/var/www/certbot"
+WEBROOT_PATH="/root/certbot"
+
+# Prepare the -d options for Certbot
+DOMAINS_OPTION="-d $DOMAIN"
+for SUBDOMAIN in $SUBDOMAINS; do
+    DOMAINS_OPTION="$DOMAINS_OPTION -d $SUBDOMAIN"
+done
 
 # Run Certbot
-certbot certonly --webroot -w $WEBROOT_PATH -d $DOMAIN --email $EMAIL --agree-tos --no-eff-email
-
+certbot certonly --webroot -w $WEBROOT_PATH $DOMAINS_OPTION --email $EMAIL --agree-tos --no-eff-email
