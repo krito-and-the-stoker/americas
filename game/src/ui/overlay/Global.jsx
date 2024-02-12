910e0cc5674989e8e122ef23a16d59f2b6716645
Christoph Franke <hallo@christophfranke.info>
2024-02-12 13:57:41 +0100

    added speed

--- game/src/ui/overlay/Global.jsx
+++ game/src/ui/overlay/Global.jsx
@@ -1,14 +1,16 @@
-import { Show } from 'solid-js'
+import { Show, createSignal } from 'solid-js'
 import Overlay from 'ui/overlay'
 import style from './Global.module.scss'
 
 import Time from 'timeline/time'
 import Binding from 'ui/binding'
 import Treasure from 'entity/treasure'
+import Europe from 'view/europe'
 
 import GameIcon from 'ui/components/GameIcon'
 
 function Global() {
+	const [speed, setSpeed] = createSignal(3)
 	const [treasure, setTreasure] = Binding.create(
 		Treasure.listen.amount,
 		Treasure.update.amount
@@ -22,9 +24,17 @@ function Global() {
 
 	return <Show when={Overlay.isVisible()}>
 		<div class={style.main}>
-			<br /><span>{month()} {dayOfMonth()}, A.D. {year()}</span>
-			Treasure: {Math.round(treasure())}<GameIcon name="gold" scale={0.8} />
-			<br /><button onClick={() => setTreasure(x => x + 10)}>more</button>
+			<div>{month()} {dayOfMonth()}, A.D. {year()}</div>
+			<div>Treasure: {Math.round(treasure())}<GameIcon name="gold" scale={0.8} /></div>
+			<div><button onClick={() => setTreasure(x => x + 10)}>more</button></div>
+			<div class={style.speed}>
+				<span onClick={() => Time.togglePause()}>{paused() ? '>' : '||'}</span>
+				<span class={speed() === 1 ? style.selected : null}>1</span>
+				<span class={speed() === 2 ? style.selected : null}>2</span>
+				<span class={speed() === 3 ? style.selected : null}>3</span>
+				<span class={speed() === 4 ? style.selected : null}>4</span>
+				<span class={speed() === 5 ? style.selected : null}>5</span>
+			</div>
 		</div>
 	</Show>
 }
