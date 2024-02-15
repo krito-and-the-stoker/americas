b84c6af993a95864e5005397d6d4919b5eaaf7e0
Christoph Franke <hallo@christophfranke.info>
2024-02-15 10:37:35 +0100

    renamed signal parameters

--- game/src/game/util/signal.js
+++ game/src/game/util/signal.js
@@ -4,13 +4,13 @@ import Util from 'util/util'
 import Binding from 'util/binding'
 
 
-function create(listener) {
-  if (Array.isArray(listener)) {
-    return listener.map(create)
+function create(listenerNoInput) {
+  if (Array.isArray(listenerNoInput)) {
+    return listenerNoInput.map(create)
   }
 
   const [signal, setSignal] = createSignal(undefined, { equals: false })
-  const cleanup = listener(value => {
+  const cleanup = listenerNoInput(value => {
     setSignal(value)
   })
 
@@ -20,47 +20,40 @@ function create(listener) {
   return signal
 }
 
-function chain(listener, listenerWithInput) {
-  if (Array.isArray(listener)) {
-    return listener.map(l => chain(l, listenerWithInput))
+function chain(listenerNoInput, listenerWithInput) {
+  if (Array.isArray(listenerNoInput)) {
+    return listenerNoInput.map(l => chain(l, listenerWithInput))
   }
 
   if (Array.isArray(listenerWithInput)) {
-    return listenerWithInput.map(i => chain(listener, i))
+    return listenerWithInput.map(k => chain(listenerNoInput, k))
   }
 
-  return fn => listener(value => value && listenerWithInput(value, fn))
+  // the chaining creates a listener without input
+  return fn => listenerNoInput(value => value && listenerWithInput(value, fn))
 }
 
-function bind(entity, listener) {
-  return listener.bind(null, entity)
+function bind(entity, listenerWithInput) {
+  return listenerWithInput.bind(null, entity)
 }
 
-function map(listener, mapping) {
-  if (Array.isArray(listener)) {
-    return listener.map(l => map(l, mapping))
+function map(listenerMaybeInput, mapping) {
+  if (Array.isArray(listenerMaybeInput)) {
+    return listenerMaybeInput.map(l => map(l, mapping))
   }
 
   if (Array.isArray(mapping)) {
-    return mapping.map(m => map(listener, m))
+    return mapping.map(m => map(listenerMaybeInput, m))
   }
 
   // There are two call signatures of listeners:
-  // 1. listener(fn) // no argument
-  // 2. listener(entity, fn) // with argument
+  // 1. listener(fn) // no input
+  // 2. listener(entity, fn) // with input
   return (arg0, arg1) => Util.isFunction(arg0)
-    ? listener(Binding.map(mapping, arg0))
-    : listener(arg0, Binding.map(mapping, arg1))
+    ? listenerMaybeInput(Binding.map(mapping, arg0))
+    : listenerMaybeInput(arg0, Binding.map(mapping, arg1))
 }
 
-
-// let's see...
-function createEntity(entity, listeners) {
-  return Object.fromEntries(
-    Object.entries(listeners)
-      .map(([key, listener]) => ([key, createAt(key, listener)]))
-    )
-}
 
 export default {
   create,
