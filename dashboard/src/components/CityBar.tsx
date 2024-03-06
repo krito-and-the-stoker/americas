fa627d76d24ef4e9255a5eda085d1d5dfb1907bf
Christoph Franke <hallo@christophfranke.info>
2024-03-06 16:34:57 +0100

    sort cities by total amount of events, limit to 20 entries

--- dashboard/src/components/CityBar.tsx
+++ dashboard/src/components/CityBar.tsx
@@ -31,46 +31,64 @@ function CityBar() {
         }
     })
 
-    const eventData = () => {
+    const sortedData = () => {
         const counts = data()?.countByCity
         if (!counts) {
             return null
         }
 
+        return Object.keys(counts).sort((a, b) => {
+            return Object.values(counts[b]).reduce((acc, cur) => acc + cur, 0) - Object.values(counts[a]).reduce((acc, cur) => acc + cur, 0)
+        }).map(key => {
+            return {
+                city: key,
+                count: counts[key]
+            }
+        }).slice(0, 20)
+    }
+
+
+    const eventData = () => {
+        const baseData = sortedData()
+        if (!baseData) {
+            return
+        }
+        const cities = baseData.map(entry => entry.city)
+
         return {
-            labels: Object.keys(counts),
+            labels: cities,
             datasets: [
                 {
                     label: 'Page Views',
-                    data: Object.values(counts).map(count => count.PageView),
+                    data: baseData.map(entry => entry.count).map(count => count.PageView),
                     backgroundColor: 'rgba(37, 150, 190, 0.5)',
                     borderColor: 'rgba(37, 150, 190, 1)',
                     borderWidth: 1,
                 },
                 {
                     label: 'New Games',
-                    data: Object.values(counts).map(count => count.NewGame),
+                    data: baseData.map(entry => entry.count).map(count => count.NewGame),
                     backgroundColor: 'rgba(70, 250, 217, 0.5)',
                     borderColor: 'rgba(70, 250, 217, 1)',
                     borderWidth: 1,
                 },
                 {
                     label: 'Resume Games',
-                    data: Object.values(counts).map(count => count.ResumeGame),
+                    data: baseData.map(entry => entry.count).map(count => count.ResumeGame),
                     backgroundColor: 'rgba(36, 179, 121, 0.5)',
                     borderColor: 'rgba(36, 179, 121, 1)',
                     borderWidth: 1,
                 },
                 {
                     label: 'Auto Saves',
-                    data: Object.values(counts).map(count => count.AutoSave),
+                    data: baseData.map(entry => entry.count).map(count => count.AutoSave),
                     backgroundColor: 'rgba(250, 226, 108, 0.5)',
                     borderColor: 'rgba(250, 226, 108, 1)',
                     borderWidth: 1,
                 },
                 {
                     label: 'Errors',
-                    data: Object.values(counts).map(count => count.Error),
+                    data: baseData.map(entry => entry.count).map(count => count.Error),
                     backgroundColor: 'rgba(175, 38, 38, 0.5)',
                     borderColor: 'rgba(175, 38, 38, 1)',
                     borderWidth: 1,
