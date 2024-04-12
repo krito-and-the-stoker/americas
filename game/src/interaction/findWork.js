cc23c8dfe6cde2b2a750a91ebd99b9d6ab1be0c0
Christoph Franke <hallo@christophfranke.info>
2024-04-12 18:27:28 +0200

    start production with wood when founding colony

--- game/src/interaction/findWork.js
+++ game/src/interaction/findWork.js
@@ -2,6 +2,7 @@ import Tile from 'entity/tile'
 import MapEntity from 'entity/map'
 import Colonist from 'entity/colonist'
 
+const initialGood = 'wood'
 export default colonist => {
   const colony = colonist.colony
   const tile = MapEntity.tile(colony.mapCoordinates)
@@ -9,7 +10,7 @@ export default colonist => {
     .filter(neighbor => !neighbor.harvestedBy)
     .reduce(
       (winner, neighbor) => {
-        const production = Tile.production(neighbor, 'food', colonist)
+        const production = Tile.production(neighbor, initialGood, colonist)
         return production > winner.production
           ? {
               production,
@@ -20,7 +21,7 @@ export default colonist => {
       { production: -1 }
     )
   if (winner.tile) {
-    Colonist.beginFieldWork(colonist, winner.tile, 'food')
+    Colonist.beginFieldWork(colonist, winner.tile, initialGood)
   } else {
     Colonist.beginColonyWork(colonist, colony.newBuildings.find(building => building.name === 'carpenters'))
   }
