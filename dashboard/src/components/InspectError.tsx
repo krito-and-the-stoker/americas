be5b49717fb750fe2971480fb5a64b687e38bae9
Christoph Franke <hallo@christophfranke.info>
2024-08-21 00:09:18 +0200

    enable to open error on localhost

--- dashboard/src/components/InspectError.tsx
+++ dashboard/src/components/InspectError.tsx
@@ -1,16 +1,19 @@
 import { createSignal, Show } from 'solid-js'
 
-// const host = window.location.hostname === 'localhost' ? 'http://localhost:3000' : 'https://www.play-americas.com'
-const host = 'http://localhost:3000' // always open on localhost
+const host = window.location.hostname === 'localhost' ? 'http://localhost:3000' : 'https://www.play-americas.com'
 
 async function fetchGameData(id: string) {
-
     const response = await fetch('/api/error/get/' + id)
     const data = await response.json()
 
-    console.log(data)
-
     return data.error?.savegame
+}
+
+async function fetchGameId(id: string) {
+    const response = await fetch('/api/error/get/' + id)
+    const data = await response.json()
+
+    return data.error?.gameid
 }
 
 async function createGame(data: string) {
@@ -26,7 +29,7 @@ async function createGame(data: string) {
 
 }
 
-async function openGame(id: string) {
+async function openGameServer(id: string) {
     setIsOpening(true)
     const data = await fetchGameData(id)
     const response = await createGame(data)
@@ -37,6 +40,16 @@ async function openGame(id: string) {
     }
 }
 
+async function openGameLocal(id: string) {
+    setIsOpening(true)
+    const gameId = await fetchGameId(id)
+
+    setIsOpening(false)
+
+    if (gameId) {
+        window.open(`http://localhost:3000/game/${gameId}`)
+    }
+}
 
 const [isOpening, setIsOpening] = createSignal(false)
 
@@ -45,11 +58,18 @@ function InspectError(props: any) {
         <h1>Error ID: {props.params.id}</h1>
         <div>
             <button
-                class="bg-blue-500 text-white font-bold py-2 px-4 rounded my-12"
+                class="bg-blue-500 text-white font-bold py-2 px-4 rounded my-12 mx-2"
                 classList={{'opacity-50': isOpening(), 'cursor-not-allowed': isOpening(), 'hover:bg-blue-700': !isOpening()}}
-                onClick={() => !isOpening() && openGame(props.params.id)}
+                onClick={() => !isOpening() && openGameServer(props.params.id)}
                 disabled={isOpening()}>
-                Open Game
+                Open Game on Server
+            </button>
+            <button
+                class="bg-blue-500 text-white font-bold py-2 px-4 rounded my-12 mx-2"
+                classList={{'opacity-50': isOpening(), 'cursor-not-allowed': isOpening(), 'hover:bg-blue-700': !isOpening()}}
+                onClick={() => !isOpening() && openGameLocal(props.params.id)}
+                disabled={isOpening()}>
+                Open Game on localhost:3000
             </button>
             <Show when={isOpening()}><div>Loading game...</div></Show>
         </div>
