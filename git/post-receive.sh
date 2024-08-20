70bc8eb9592dddb8580bff9c1e3efff0e8ab4a2d
Christoph Franke <hallo@christophfranke.info>
2024-08-20 23:09:22 +0200

    changed git post receive hook

--- git/post-receive.sh
+++ git/post-receive.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
+target_directory="/root/volume/live"
+log_file="/root/volume/git-receive.log"
+
 while read oldrev newrev refname
 do
     # Check if the received branch is 'live'
     if [ "$refname" = "refs/heads/live" ]; then
-        # Specify the target directory
-        target_directory="/root/live"
-
-        # Specify the log file
-        log_file="/root/git-receive.log"
-
         # Redirect output to the log file
         exec >> "$log_file" 2>&1
 
@@ -26,8 +23,12 @@ do
         # Get the commit message of the new revision
         commit_message=$(git log --format=%B -n 1 $newrev)
         echo "[$(date)] Live branch content copied to $target_directory. Commit message: '$commit_message'"
-        
+
+        # Copy the update-containers.sh script to parent directory
+        cp $target_directory/server/update-containers.sh $target_directory/..
+
         # Execute any additional container update scripts in the background
-        /root/update-containers.sh &
+        cd $target_directory/..
+        ./update-containers.sh &
     fi
 done
