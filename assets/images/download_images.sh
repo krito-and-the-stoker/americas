9597126a3fc08a9a04888e42b4a27387f64fbcd1
Christoph Franke <hallo@christophfranke.info>
2024-09-20 20:54:39 +0200

    only download images, nothing else

--- assets/images/download_images.sh
+++ assets/images/download_images.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -avzP root@164.92.193.78:/root/americas-images/ ./
+rsync -avzP --include='*/' --include='*.jpg' --include='*.jpeg' --include='*.png' --exclude='*' root@164.92.193.78:/root/americas-images/ ./
