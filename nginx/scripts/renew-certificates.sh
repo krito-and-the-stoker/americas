b8894ac85dd7496d44486f8d67950e33a19127af
Christoph Franke <hallo@christophfranke.info>
2025-07-16 12:26:20 +0200

    change back proxy from china to default

--- nginx/scripts/renew-certificates.sh
+++ nginx/scripts/renew-certificates.sh
@@ -9,3 +9,6 @@ certbot renew --webroot -w $WEBROOT_PATH --email $EMAIL --agree-tos --no-eff-ema
 
 # Reload Nginx to apply the renewed certificate (optional, uncomment if needed)
 nginx -s reload
+
+# usage:
+# docker exec live-webserver-1 renew-certificates.sh
